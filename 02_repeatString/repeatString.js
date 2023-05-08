1a10c6d87939341fa517c2b4c8bdd364c7483316
TumarreTorre <marcgonzaleztarrio@gmail.com>
2023-05-08 23:49:46 +0200

    wqe

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function (text, n) {
+  if (n < 0) return "ERROR";
+  return text.repeat(n);
 };
-
 // Do not edit below this line
 module.exports = repeatString;
